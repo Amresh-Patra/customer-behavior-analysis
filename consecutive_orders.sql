d50fa11f4ac6f10a7d7d45e53d86cdfad3e51702
Amresh-Patra <amreshpatra2004@gmail.com>
2026-05-02 19:28:22 +0530

    Update consecutive_orders.sql
    Fix and update consecutive orders query

--- consecutive_orders.sql
+++ consecutive_orders.sql
@@ -1,1 +1,14 @@
+WITH temp AS (
+    SELECT 
+        customer_id,
+        order_date,
+        LAG(order_date) OVER (
+            PARTITION BY customer_id 
+            ORDER BY order_date
+        ) AS prev_date
+    FROM orders
+)
 
+SELECT DISTINCT customer_id
+FROM temp
+WHERE DATEDIFF(order_date, prev_date) = 1;
